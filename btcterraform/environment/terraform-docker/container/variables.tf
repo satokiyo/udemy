20cb61d4779cde557a01dc53a66b61068e50636b
satokiyo <k.sato.ontology@gmail.com>
2022-03-27 10:45:20 +0900

    rm

--- btcterraform/environment/terraform-docker/container/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "name_in" {}
-variable "image_in" {}
-variable "int_port_in" {}
-variable "ext_port_in" {}
-variable "container_path_in" {}
-variable "host_path_in" {}
--- btcterraform/environment/terraform-docker/image/main.tf
+++
@@ -1,4 +0,0 @@
-resource "docker_image" "nodered_image" {
-  //name = "nodered/node-red:latest"
-  name = var.image_in // module側から参照する、ルート側で定義したvariable
-}
